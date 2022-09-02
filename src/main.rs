2aa3854430fb94600a05ee3b0df4b8ed5440506f
J Semidey <mtsemidey@gmail.com>
2022-09-02 16:45:08 -0400

    threads to handle game and render loop

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use std::error::Error;
+use std::thread;
 use std::io;
+use std::sync::mpsc;
 use std::time::Duration;
 use crossterm::event::{KeyCode, Event};
 use crossterm::{terminal, ExecutableCommand, event};
 use crossterm::terminal::{EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::cursor::{Hide, Show};
+use invaders::frame;
+use invaders::frame::new_frame;
+use invaders::render;
 use rusty_audio::Audio;
 
 fn main() -> Result <(), Box<dyn Error>> {
@@ -12,7 +17,6 @@ fn main() -> Result <(), Box<dyn Error>> {
     let mut audio = Audio::new();
 
     // add all audio sources to audio manager
-    // (name of file, path of audio file)
     audio.add("explode", "explode.wav");
     audio.add("lose", "lose.wav");
     audio.add("move", "move.wav");
@@ -32,8 +36,41 @@ fn main() -> Result <(), Box<dyn Error>> {
     // [crossterm] hide cursor when entering alternate screen
     stdout.execute(Hide)?;
 
+    // RENDER LOOP in a separate thread (for speed optimization)
+    // channel to communicate with threads
+    // (render transciever and render receiver) using mspc channels 
+    let (render_tx, render_rx) = mpsc::channel();
+    
+    // THREAD: recieve frames and render them
+    // catch thread handle (render_handle) with closure
+    // move || closure captures end of channel
+    let render_handle = thread::spawn(move || {
+        // variable to hold last frame (new empty frame)
+        let mut last_frame = frame::new_frame();
+        // render to standard output with new access to stdout
+        let mut stdout = io::stdout();
+        // render entire screen once with force rendering
+        render::render(&mut stdout, &last_frame, &last_frame, true);
+
+        loop {
+            // set curr_frame to result of match, either valid Frame (x) or break loop on error
+            // on error (shuts down child thread)
+            let curr_frame = match render_rx.recv() {
+                Ok(x) => x,
+                Err(_) => break,
+            };
+            // render frame with references to last frame, current frame and force set to false
+            render::render(&mut stdout, &last_frame, &curr_frame, false);
+            // housekeeping: last frame is now current frame to set up for next loop
+            last_frame = curr_frame;
+        } 
+    });
+
     // MAIN GAME LOOP
     'gameloop: loop {
+        // Per-frame initialization
+        let curr_frame = new_frame();
+
         // INPUT HANDLING
         // poll for input events, duration set to default (immediate)
         while event::poll(Duration::default())? {
@@ -50,9 +87,26 @@ fn main() -> Result <(), Box<dyn Error>> {
                 }
             }
         }
+
+        // DRAW & RENDER
+        // send frame, .send moves to a different thread
+        // expects to fail first few times because game loop starts before child thread
+        // starts recieving. ignore it silently let _ (wildcard)
+        let _ = render_tx.send(curr_frame);
+
+        // game loop is faster than render loop
+        // artificial sleep adds limit to no more than generating 1000 fps to avoid
+        // falling behind when rendering
+        thread::sleep(Duration::from_millis(1));
+
     }
 
     // CLEANUP
+    // drop transmitting side of the channel, causes recieving channel to error and end loop 
+    // becoming safe to join threads
+    drop(render_tx);
+    render_handle.join().unwrap();
+
     // block until all audio is done playing with wait()
     audio.wait();
     // show cursor, leave alternate screen, and disable raw mode
